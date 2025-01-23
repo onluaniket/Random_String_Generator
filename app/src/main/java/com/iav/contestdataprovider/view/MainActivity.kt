d6ebc3bd4480fd2005e85b03ade383477daf760e
AniGaikwad <aniket.gaikwad@stratos.studio>
2025-01-24 00:45:58 +0530

    Random String Generator Code Add Apk File

--- app/src/main/java/com/iav/contestdataprovider/view/MainActivity.kt
+++ app/src/main/java/com/iav/contestdataprovider/view/MainActivity.kt
@@ -2,6 +2,7 @@ package com.iav.contestdataprovider.view
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
@@ -37,9 +38,23 @@ class MainActivity : AppCompatActivity() {
 
     fun onGenerateClick(view: View) {
         val length = binding.etLength.text.toString().toIntOrNull()
-        length?.let {
-            viewModel.fetchRandomString(it)
+
+        when {
+            length == null -> {
+                Toast.makeText(this, "Please enter Length number", Toast.LENGTH_SHORT).show()
+            }
+
+            length <= 0 -> {
+                Toast.makeText(this, "Please enter a positive Length", Toast.LENGTH_SHORT).show()
+            }
+
+            else -> {
+                length?.let {
+                    viewModel.fetchRandomString(it)
+                }
+            }
         }
+
     }
 
     fun onClearAllClick(view: View) {
